dd9426dce9a3a82d6877fd6a5c790c8174496d75
leonardo fraquelli <leonardo.fraquelli1997@gmail.com>
2023-10-25 22:06:34 +0200

    balance cli

---
+++ avalancheRPC/experiments/balanceCli.js
@@ -0,0 +1,39 @@
+// const sendJsonRpcRequest = require('./sendJsonRpcRequest');
+import { requestProcessor } from '../utils/requestProcessor.mjs';
+import { loadOrGenerateUserInfo, verifyUserInfoHasAccount } from '../utils/loadOrGenerateUserInfo.mjs';
+
+import { setupKeys } from '../utils/knownAddresses.mjs';
+import { verifyBalance } from '../utils/verifyBalance.mjs';
+
+
+// Parse command-line arguments to extract the methodName and additional parameters
+
+const assetID = "2fombhL7aGPwj3KH4bfrmJwW6PVnMobf9Y2fn9GwxiAAJyFDbe"
+
+let addr;
+let chain = x;
+function parseCommandLineArgs() {
+    addr = process.argv[2];
+    chain = process.argv[3];
+}
+
+
+
+const main = async () => {
+    try {
+
+        parseCommandLineArgs()
+        balance = await verifyBalance(chain, addr)
+        console.log("Balance:", balance)
+    } catch (error) {
+        console.error('Error occurred:', error.message);
+        // Continue with your error handling logic, if needed
+    }
+};
+
+
+main()
+
+
+
+
