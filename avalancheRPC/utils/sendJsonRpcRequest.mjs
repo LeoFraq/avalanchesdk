09de72227829a62a9edd6b35e61a23c09aa93b52
leonardo fraquelli <leonardo.fraquelli1997@gmail.com>
2023-11-13 21:37:11 +0100

    random IPs in sendjsonrpc

--- avalancheRPC/utils/sendJsonRpcRequest.mjs
+++ avalancheRPC/utils/sendJsonRpcRequest.mjs
@@ -1,7 +1,10 @@
 import axios from "axios"
+import fs from "fs";
 
-const IP = '127.0.0.1'; // Replace with your server's IP address
-const PORT = 9650; // Replace with your server's port
+
+
+let IP = '127.0.0.1'; // Replace with your server's IP address
+let PORT = 9650; // Replace with your server's port
 const id = 1;
 const contentType = 'application/json';
 
@@ -9,7 +12,13 @@ const contentType = 'application/json';
 // "curl -X POST --data '{ "jsonrpc":"2.0", "id" :1, "method" :"health.health"}' -H 'content-type:application/json;' 127.0.0.1:9650/ext/health"
 // Function to send a JSON-RPC request
 export async function sendJsonRpcRequest(method, params, endpoint) {
-    const baseURL = `http://${IP}:${PORT}/${endpoint}`;
+    let validIP = readAvailableIPs()
+    validIP.push(IP + ":" + PORT)
+
+    // Choose a random IP from the validIPs array
+    const randomIP = validIP[Math.floor(Math.random() * validIP.length)];
+    const baseURL = `http://${randomIP}/${endpoint}`;
+
     const requestData = {
         jsonrpc: '2.0',
         id: id,
@@ -34,5 +43,28 @@ export async function sendJsonRpcRequest(method, params, endpoint) {
         // Handle errors here
         console.error(baseURL, 'Error:', error.message, "With requestData:\n", requestData);
         throw error
+    }
+}
+
+
+const readAvailableIPs = () => {
+    try {
+        const data = fs.readFileSync('IP.json', 'utf8');
+        const jsonData = JSON.parse(data);
+        const bootstrapIps = jsonData['bootstrap-ips'];
+
+        // Split the string into an array of IP:Port values
+        const ipsArray = bootstrapIps.split(',');
+
+        // Do something with the array of IPs, for example, log them
+        console.log('Available IPs:');
+        ipsArray.forEach(ip => {
+            console.log(ip);
+        });
+
+        return ipsArray;
+    } catch (error) {
+        console.error('Error reading IPs from IP.json:', error.message);
+        throw error;
     }
 }
