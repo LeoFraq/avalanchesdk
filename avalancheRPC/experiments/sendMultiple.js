875948fba45e6c1e4bef24b7b9b979ffc35ca36a
leonardo fraquelli <leonardo.fraquelli1997@gmail.com>
2024-02-10 22:08:57 +0100

    kin

--- avalancheRPC/experiments/sendMultiple.js
+++ avalancheRPC/experiments/sendMultiple.js
@@ -14,7 +14,7 @@ const assetID = "2fombhL7aGPwj3KH4bfrmJwW6PVnMobf9Y2fn9GwxiAAJyFDbe"
 let methodName = 'avm.sendMultiple';
 let params = {};
 let iterations = 1
-const CHUNK_SIZE = 2000; // Define your chunk size
+const CHUNK_SIZE = 500; // Define your chunk size
 // Function to parse command-line arguments
 function parseCommandLineArgs() {
     iterations = process.argv[2];
